9af882ea70c0b6fd59f8b7ad73985bb6e3c026c8
littlePinkyDonkey <teplyh.andre@yandex.ru>
2020-12-26 17:54:12 +0300

    little problem with canvas

--- src/main/java/validators/ValidatorX.java
+++ src/main/java/validators/ValidatorX.java
@@ -6,20 +6,20 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-public class ValidatorY implements Validator {
+public class ValidatorX implements Validator {
 
     @Override
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object value) throws ValidatorException {
         try {
             double parsedValue = Double.parseDouble(value.toString());
-            if (!(parsedValue > -3 && parsedValue < 5)) {
+            if (!(parsedValue > -3 && parsedValue < 3)) {
                 FacesMessage message = new FacesMessage(
-                        "Введено некорректное значение Y",
-                        "Диапазон Y:(-3 ... 5)");
+                        "Введено некорректное значение X",
+                        "Диапазон X:(-3 ... 3)");
                 throw new ValidatorException(message);
             }
         } catch (NumberFormatException e) {
-            FacesMessage message = new FacesMessage("Введено некорректное значение Y", "Y должен быть числом");
+            FacesMessage message = new FacesMessage("Введено некорректное значение X", "X должен быть числом");
             throw new ValidatorException(message);
         }
     }

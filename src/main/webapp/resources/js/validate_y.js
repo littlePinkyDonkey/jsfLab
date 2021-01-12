abee23b913c41ac39952b99421820dd30f88b814
littlePinkyDonkey <teplyh.andre@yandex.ru>
2021-01-12 20:39:36 +0300

    last chenges

--- src/main/webapp/resources/js/validate_y.js
+++ src/main/webapp/resources/js/validate_y.js
@@ -1,8 +1,28 @@
 function selectY(checkBox, value) {
+    let count = 0;
     let boxes = document.getElementsByClassName("y-box");
     for (let box of boxes) {
+        if (box.checked === true) {
+            count++;
+        }
         box.checked = false;
+    }
+    if (count > 0) {
+        document.getElementById("warning-container-Y").innerText = "";
     }
     checkBox.checked = true;
     document.getElementById("computation-form:y").value = value;
 }
+
+window.onload = () => {
+    let count = 0;
+    let boxes = document.getElementsByClassName("y-box");
+    for (let box of boxes) {
+        if (box.checked === true) {
+            count++;
+        }
+    }
+    if (count > 0) {
+        document.getElementById("warning-container-Y").innerText = "";
+    }
+};
